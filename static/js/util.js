a20886242f58ae1a44ee8323d2e33e6f85eca57c
me38033827 <183225137@qq.com>
2019-05-23 19:13:35 -0700

    weather

--- static/js/util.js
+++ static/js/util.js
@@ -68,3 +68,26 @@ function drawPoint(point){
   var ctx=c.getContext("2d");
   ctx.fillRect(point[0],point[1],2,2);
 }
+
+
+
+var week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+function show_Weather() {
+    $.ajax({
+        url: '/weather',
+        type: 'GET',
+        success: function(data, textStatus, request) {
+            $("#temp").html(data["main"]["temp"] + "<sup>°F</sup>");
+            $("#weather").html(data["weather"][0]["main"]);
+            var d = new Date();
+            var n = d.getDay();
+            $("#Today").html(week[n]);
+            $("#date").html(d.toDateString());
+            $("#wind").html(data["wind"]["speed"] + "km/h");
+            d = new Date(data["sys"]["sunrise"] * 1000);
+            $("#sunrise").html(d.toTimeString());
+            $("#pressure").html(data["main"]["pressure"] + "hPa");
+            $("#weatherIcon").attr("src", "https://openweathermap.org/img/w/" + data["weather"][0]["icon"] + ".png")
+        }
+    });
+}
